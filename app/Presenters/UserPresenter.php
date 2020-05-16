3b714a5eca5312a7242d0503975f05524a65b725
Kammet07 <pavel.pupentsov@educanet.cz>
2020-05-17 01:26:11 +0200

    time to sleep 1

--- app/Presenters/UserPresenter.php
+++ app/Presenters/UserPresenter.php
@@ -7,13 +7,14 @@ namespace App\Presenters;
 use Nette;
 use Nette\Application\UI;
 use Tracy\Debugger;
+use Nette\Security\User;
 
 final class UserPresenter extends Nette\Application\UI\Presenter
 {
     protected function createComponentRegistrationForm(): UI\Form
     {
         $form = new UI\Form;
-        $form->addText('name', 'Jméno:');
+        $form->addText('username', 'Jméno:');
         $form->addPassword('password', 'Heslo:');
         $form->addSubmit('login', 'Registrovat');
         $form->onSuccess[] = [$this, 'registrationFormSucceeded'];
@@ -23,14 +24,14 @@ final class UserPresenter extends Nette\Application\UI\Presenter
     // volá se po úspěšném odeslání formuláře
     public function registrationFormSucceeded(UI\Form $form, \stdClass $values): void
     {
-        $this->flashMessage('Byl jste úspěšně registrován. ' . $values->name);
+        $this->flashMessage('Byl jste úspěšně registrován. ' . $values->username);
         $this->redirect('Lorem:lorem');
     }
 
     protected function createComponentLoginForm(): UI\Form
     {
         $form = new UI\Form;
-        $form->addText('name', 'Jméno:');
+        $form->addText('username', 'Jméno:');
         $form->addPassword('password', 'Heslo:');
         $form->addSubmit('login', 'Login');
         $form->onSuccess[] = [$this, 'loginFormSucceeded'];
@@ -42,8 +43,15 @@ final class UserPresenter extends Nette\Application\UI\Presenter
     {
         Debugger::barDump($values);
 
-        $this->flashMessage('Byl jste úspěšně prihlasen. ' . $values->name);
-        $this->redirect('Lorem:lorem');
+        try {
+            $this->getUser()->login($values->username, $values->password);
+
+            $this->redirect('Lorem:lorem');
+            $this->flashMessage('Byl jste úspěšně prihlasen. ' . $values->username);
+
+        } catch (Nette\Security\AuthenticationException $e) {
+            $form->addError('Špatný jméno nebo heslo.');
+        }
     }
 
 }
